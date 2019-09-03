9e6b8b615e3b39a9d3280fc064cfaef7c5b8092a
diegchav <diegchav.me@gmail.com>
2019-09-03 16:41:05 -0500

    Updates

---
+++ precache-manifest.8982bea84594f28d889ae0c7600a20f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6e2ea64da152f5a8b99b9356a47fb9",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "34e41abcecfde0067108",
+    "url": "/game-of-life/static/css/main.8b6fb320.chunk.css"
+  },
+  {
+    "revision": "ce300c39650df6c68da0",
+    "url": "/game-of-life/static/js/2.1aef73fa.chunk.js"
+  },
+  {
+    "revision": "34e41abcecfde0067108",
+    "url": "/game-of-life/static/js/main.0e178ae0.chunk.js"
+  },
+  {
+    "revision": "7fe30b866842a34fccb6",
+    "url": "/game-of-life/static/js/runtime~main.4f49a115.js"
+  }
+]);
