cbda5e8d4fa8e71d835d357a50b37ee97af5b5c4
diegchav <diegchav.me@gmail.com>
2019-09-08 21:50:14 -0500

    Remove box shadow from GameLayout component

--- src/components/styles/StyledGameLayout.js
+++ src/components/styles/StyledGameLayout.js
@@ -6,15 +6,15 @@ const StyledGameLayout = styled.div`
   align-items: center;
   justify-content: center;
   width: 70vw;
-  -webkit-box-shadow: 0 0 8px 8px rgba(${props => props.theme.rgbBoxShadow}, 0.5);
+  /* -webkit-box-shadow: 0 0 8px 8px rgba(${props => props.theme.rgbBoxShadow}, 0.5);
   -moz-box-shadow: 0 0 8px 8px rgba(${props => props.theme.rgbBoxShadow}, 0.5);
-  box-shadow: 0 0 8px 8px rgba(${props => props.theme.rgbBoxShadow}, 0.5);
+  box-shadow: 0 0 8px 8px rgba(${props => props.theme.rgbBoxShadow}, 0.5); */
 `;
 
-StyledGameLayout.defaultProps = {
-  theme: {
-    rgbBoxShadow: '0, 54, 58'
-  }
-};
+// StyledGameLayout.defaultProps = {
+//   theme: {
+//     rgbBoxShadow: '0, 54, 58'
+//   }
+// };
 
 export default StyledGameLayout;
