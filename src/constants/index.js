118e01bf2dced1d1fe8f43dd614752fcf7d18332
diegchav <diegchav.me@gmail.com>
2019-09-08 20:51:20 -0500

    Add theme palette constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,20 @@
+// Grid
 export const MIN_GRID_SIZE = 10;
 export const MAX_GRID_SIZE = 50;
 export const GRID_SIZE = 30;
 export const GRID_SIZE_STEP = 10;
 
+// Update speed
 export const MIN_UPDATE_INTERVAL = 100;
 export const MAX_UPDATE_INTERVAL = 900;
 export const UPDATE_INTERVAL = 500;
 export const UPDATE_INTERVAL_STEP = 200;
+
+// Theme
+export const THEME_PALETTE = {
+  green: {
+    primary: '#00838f',
+    secundaryLight: '#4fb3bf',
+    secundaryDark: '##005662'
+  }
+};
